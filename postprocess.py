1b99b16c43a084bc1a4ac8b9250d35bab38250c0
Ray <rxian@users.noreply.github.com>
2023-04-24 22:05:37 -0500

    Switch solver; experiment changes (random splits)
    - switched to `cvxpy`, and `cpc` lp solver... should be MUCH faster now :O
    - results are averaged over 10 random splits
    - changed to logistic regression predictors from linear regression (more common)
    - changed to group-weighted error rate from group-balanced

--- postprocess.py
+++ postprocess.py
@@ -1,11 +1,44 @@
+from collections import defaultdict, deque
 from itertools import chain
+import warnings
 
 import numpy as np
-from qpsolvers import solve_ls
-from scipy.optimize import linprog
-from scipy.sparse import csc_matrix
+import cvxpy as cp
 from sklearn.base import BaseEstimator
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+
+
+class UndirectedGraph():
+  """Undirected graph data structure with basic features."""
+
+  def __init__(self, edges=[]):
+    self.adjacency_list = defaultdict(set)
+    self.add_edges(edges)
+
+  def get_nodes(self):
+    return self.adjacency_list.keys()
+
+  def get_neighbors(self, node):
+    return self.adjacency_list[node]
+
+  def add_edges(self, edges):
+    for a, b in edges:
+      self.adjacency_list[a].add(b)
+      self.adjacency_list[b].add(a)
+
+  def connected(self, a, b):
+    # BFS to check if two nodes are connected
+    visited = set()
+    queue = deque([a])
+    while queue:
+      node = queue.popleft()
+      if node == b:
+        return True
+      visited.add(node)
+      for neighbor in self.get_neighbors(node):
+        if neighbor not in visited:
+          queue.append(neighbor)
+    return False
 
 
 class PostProcessorDP(BaseEstimator):
@@ -28,7 +61,14 @@ class PostProcessorDP(BaseEstimator):
       Class assignments of each post-processed training example (unnormalized).
   """
 
-  def fit(self, scores, groups, alpha=0.0, w=None, r=None, q_by_group=None):
+  def fit(self,
+          scores,
+          groups,
+          alpha=0.0,
+          w=None,
+          r=None,
+          q_by_group=None,
+          tol=1e-8):
     """Estimate a post-processing map.
 
     Args:
@@ -43,11 +83,11 @@ class PostProcessorDP(BaseEstimator):
         Weight of each group for weighting classification error (need not be
         normalized).  Default is uniform (group-balanced).
       r: array-like, shape (n_examples,), optional
-        Probability mass of each example (need not be normalized, e.g., to avoid
-        underflow).  Default is uniform.
+        Instance weight; probability mass of each example (need not be
+        normalized).  Default is uniform.
       q_by_group: list of array-like, shape (n_classes,), optional
         Specify desired distributions of class assignments of each group.
-    """
+      """
     scores, groups = check_X_y(scores, groups)
     if r is not None:
       _, r = check_X_y(scores, r)
@@ -56,240 +96,190 @@ class PostProcessorDP(BaseEstimator):
     self.n_groups_ = int(1 + np.max(groups))
     self.alpha_ = alpha
     if w is None:
-      w = [1.0 for _ in range(self.n_groups_)]
+      w = np.bincount(groups, minlength=self.n_groups_) / len(groups)
     self.w_ = w
 
     scores_by_group = [scores[groups == a] for a in range(self.n_groups_)]
-    r_by_group = [(r[groups == a] if r is not None else np.ones(
-        (groups == a).sum())) for a in range(self.n_groups_)]
+    r_by_group = []
+    for a in range(self.n_groups_):
+      if r is not None:
+        this_r = r[groups == a]
+        # Upscale to prevent underflow
+        this_r *= len(this_r) / this_r.sum()
+        r_by_group.append(this_r)
+      else:
+        r_by_group.append(np.ones((groups == a).sum()))
+    total_r_max = max(len(r) for r in r_by_group)
 
-    self.score_, self.q_by_group_, self.gamma_by_group_ = self.linprog_dp_(
-        scores_by_group,
-        alpha=alpha,
-        w=w,
-        r_by_group=r_by_group,
-        q_by_group=q_by_group)
-    self.psi_by_group_ = np.stack([
-        self.find_point_(scores_by_group[a], self.gamma_by_group_[a])
-        for a in range(self.n_groups_)
-    ])
+    problem = self.linprog_dp_(scores_by_group,
+                               alpha=alpha,
+                               w=w,
+                               r_by_group=r_by_group,
+                               q_by_group=q_by_group)
+    problem.solve(solver=cp.CBC, integerTolerance=tol)
+
+    # Downscale, due to the upscaling to `r_by_group` above
+    self.score_ = problem.value / total_r_max
+    self.q_by_group_ = problem.var_dict["q"].value
+    self.gamma_by_group_ = [
+        problem.var_dict[f'gamma_{a}'].value for a in range(self.n_groups_)
+    ]
+
+    # self.scores_by_group_ = scores_by_group
+    psi_by_group = []
+    for a in range(self.n_groups_):
+      try:
+        problem = self.quadprog_find_point_(scores_by_group[a],
+                                            self.gamma_by_group_[a],
+                                            tol=tol)
+        problem.solve(solver=cp.OSQP)
+        z = problem.var_dict["z"].value
+        if z is None:
+          raise cp.error.SolverError
+        psi_by_group.append(
+            [0] + [2 * (z[0] - z[j]) for j in range(1, self.n_classes_)])
+      except cp.error.SolverError:
+        # This can happen when OSQP fails to converge, or `gamma_by_group_` is
+        # not optimal
+        warnings.warn("Point-finding QP failed, falling back to LP.")
+        problem = self.linprog_score_transform_(scores_by_group[a],
+                                                self.gamma_by_group_[a],
+                                                tol=tol)
+        problem.solve(solver=cp.CBC, integerTolerance=tol)
+        psi_by_group.append(2 * problem.var_dict["bias"].value)
+    self.psi_by_group_ = np.stack(psi_by_group)
+
     return self
 
   def linprog_dp_(self,
                   scores_by_group,
                   alpha,
-                  w,
-                  r_by_group,
-                  q_by_group=None,
-                  tol=1e-6):
+                  w=None,
+                  r_by_group=None,
+                  q_by_group=None):
     """This implements the LP in the paper (Line 3 of Algorithm 2)."""
 
-    # Decision variables are the probability mass of the couplings, followed by
-    # the barycenter, the output distributions, and the slack variables.
-    #
-    # They are flattened and concatenated into a single vector, which when
-    # unpacked and unraveled are tensors of shapes:
-    # - (n_examples[a], n_classes), for a = 1, ..., n_groups
-    # - (n_classes,)
-    # - (n_groups, n_classes)
-    # - (n_groups, n_classes)
+    alpha = cp.Parameter(value=alpha, name="alpha")
 
-    n_examples = [len(scores) for scores in scores_by_group]
-    total_r = [r.sum() for r in r_by_group]
-    a_max = np.argmax(total_r)
+    # Variables are the probability mass of the couplings, the barycenter,
+    # the output distributions, and slacks
+    gamma_by_group = [
+        cp.Variable(scores_by_group[a].shape, name=f"gamma_{a}")
+        for a in range(self.n_groups_)
+    ]
+    barycenter = cp.Variable(self.n_classes_, name="barycenter")
+    q = cp.Variable((self.n_groups_, self.n_classes_), name="q")
+    slack = cp.Variable((self.n_groups_, self.n_classes_), name="slack")
 
-    offset_barycenter = sum(n_examples) * self.n_classes_
-    offset_qs = offset_barycenter + self.n_classes_
-    offset_slacks = offset_qs + self.n_groups_ * self.n_classes_
-    n_variables = offset_slacks + self.n_groups_ * self.n_classes_
+    total_r = np.array([r.sum() for r in r_by_group])
 
-    def ravel_index(multi_index):
-      """Get indexes to flattened couplings given multi-index of groups, example
-         index, and classes."""
-      indices = np.empty(len(multi_index[0]), dtype=np.uint)
-      offset = 0
-      for a in range(self.n_groups_):
-        indices[multi_index[0] == a] = offset + np.ravel_multi_index(
-            [idx[multi_index[0] == a] for idx in multi_index[1:]],
-            (n_examples[a], self.n_classes_))
-        offset += n_examples[a] * self.n_classes_
-      return [int(x) for x in indices]
+    # Get l1 transportation costs
+    # Upscale to prevent underflow, avoid numerical issues, and improve run time
+    cost_by_group = [
+        (1 - scores_by_group[a]) * w[a] * total_r.max() / total_r[a] / w.sum()
+        for a in range(self.n_groups_)
+    ]
+    cost = sum([
+        cp.sum(cp.multiply(gamma_by_group[a], cost_by_group[a]))
+        for a in range(self.n_groups_)
+    ])
 
-    # l_1 transportation costs
-    cost = []
-    for a in range(self.n_groups_):
-      s = np.repeat(np.arange(n_examples[a]), self.n_classes_, axis=0)
-      y = np.tile(np.arange(self.n_classes_), n_examples[a])
-      c = 1 - scores_by_group[a][s, y]
-      # Normalize according to total p_by_group
-      c *= total_r[a_max] / total_r[a]
-      c *= w[a]
-      cost.extend(c)
-    cost = np.array(cost)
-
-    # Get constraints
-    G_i = []
-    G_j = []
-    G_v = []
-    G_rows = 0
-    h = []
-
-    A_i = []
-    A_j = []
-    A_v = []
-    A_rows = 0
-    b = []
+    # Build constraints
+    constraints = []
 
     # \sum_y \gamma_{a, s, y} = r_{a, s}
     for a in range(self.n_groups_):
-      for s in range(n_examples[a]):
-        A_i.extend([A_rows] * self.n_classes_)
-        A_j.extend(
-            ravel_index([
-                np.full(self.n_classes_, a),
-                np.full(self.n_classes_, s),
-                np.arange(self.n_classes_),
-            ]))
-        A_v.extend([1.0] * self.n_classes_)
-        A_rows += 1
-      b.extend(r_by_group[a])
+      constraints.append(cp.sum(gamma_by_group[a], axis=1) == r_by_group[a])
 
     # \sum_s \gamma_{a, s, y} = q_{a, y}
     for a in range(self.n_groups_):
-      for y in range(self.n_classes_):
-        A_i.extend([A_rows] * n_examples[a])
-        A_j.extend(
-            ravel_index([
-                np.full(n_examples[a], a),
-                np.arange(n_examples[a]),
-                np.full(n_examples[a], y),
-            ]))
-        A_v.extend([1.0] * n_examples[a])
-        A_i.append(A_rows)
-        A_j.append(offset_qs + a * self.n_classes_ + y)
-        A_v.append(-1.0 * total_r[a])
-        b.append(0.0)
-        A_rows += 1
+      constraints.append(cp.sum(gamma_by_group[a], axis=0) == q[a] * total_r[a])
 
+    # -\xi_{a, y} <= q_{a, y} - barycenter_{y} <= \xi_{a, y}
     if q_by_group is None:
-      # -\xi_{a, y} <= q_{a, y} - barycenter_{y} <= \xi_{a, y}
       for a in range(self.n_groups_):
-        for y in range(self.n_classes_):
-          for i in [-1.0, 1.0]:
-            G_i.append(G_rows)
-            G_j.append(offset_qs + a * self.n_classes_ + y)
-            G_v.append(i)
-            G_i.append(G_rows)
-            G_j.append(offset_barycenter + y)
-            G_v.append(-i)
-            G_i.append(G_rows)
-            G_j.append(offset_slacks + a * self.n_classes_ + y)
-            G_v.append(-1.0)
-            h.append(0.0)
-            G_rows += 1
-
-      # \xi_{a, y} <= \alpha / 2
-      G_i.extend(range(G_rows, G_rows + self.n_groups_ * self.n_classes_))
-      G_j.extend(
-          range(offset_slacks,
-                offset_slacks + self.n_groups_ * self.n_classes_))
-      G_v.extend([1.0] * self.n_groups_ * self.n_classes_)
-      h.extend([alpha / 2] * self.n_groups_ * self.n_classes_)
-      G_rows += self.n_groups_ * self.n_classes_
-
+        constraints.append(-slack[a] <= q[a] - barycenter)
+        constraints.append(q[a] - barycenter <= slack[a])
     else:
+      q_by_group = cp.Parameter((self.n_groups_, self.n_classes_),
+                                value=q_by_group,
+                                name="q_by_group")
       for a in range(self.n_groups_):
-        for y in range(self.n_classes_):
-          A_i.append(A_rows)
-          A_j.append(offset_qs + a * self.n_classes_ + y)
-          A_v.append(1.0)
-          b.append(q_by_group[a][y])
-          A_rows += 1
+        constraints.append(-slack[a] <= q[a] - q_by_group[a])
+        constraints.append(q[a] - q_by_group[a] <= slack[a])
 
-    # `scipy.optimize.linprog` interface
-    G = csc_matrix((G_v, (G_i, G_j)), shape=(G_rows, n_variables))
-    h = np.array(h)
-    A = csc_matrix((A_v, (A_i, A_j)), shape=(A_rows, n_variables))
-    b = np.array(b)
-    sol = linprog(np.concatenate([cost, [0.0] * (n_variables - len(cost))]),
-                  A_ub=G,
-                  b_ub=h,
-                  A_eq=A,
-                  b_eq=b,
-                  bounds=(0, None),
-                  method="highs",
-                  options={
-                      "dual_feasibility_tolerance": tol,
-                      "primal_feasibility_tolerance": tol,
-                  })
-    assert sol.status == 0, sol.message
-    x = sol.x
+    # \xi_{a, y} <= \alpha / 2
+    constraints.append(slack <= alpha / 2)
 
-    gammas_by_group_unnormalized = []
-    total_cost = 0.0
-    offset = 0
-    for a in range(self.n_groups_):
-      this_gamma = x[offset:offset + n_examples[a] * self.n_classes_]
-      gammas_by_group_unnormalized.append(
-          this_gamma.reshape((n_examples[a], self.n_classes_)))
-      c = cost[offset:offset + n_examples[a] * self.n_classes_]
-      total_cost += np.sum(this_gamma * c) / total_r[a_max]
-      offset += n_examples[a] * self.n_classes_
-    total_cost /= sum(w)
+    # All variables are nonnegative
+    constraints.extend([gamma >= 0 for gamma in gamma_by_group])
+    constraints.append(q >= 0)
+    constraints.append(barycenter >= 0)
+    constraints.append(slack >= 0)
 
-    return total_cost, [
-        x[offset_qs + a * self.n_classes_:offset_qs + (a + 1) * self.n_classes_]
-        for a in range(self.n_groups_)
-    ], gammas_by_group_unnormalized
+    return cp.Problem(cp.Minimize(cost), constraints)
 
-  def find_point_(self, scores, gamma):
+  def quadprog_find_point_(self, scores, gamma, tol=1e-8):
     """Extract post-processing map from LP solution (Lines 6-9 of Alg. 2)."""
 
-    # Compute boundaries and get constraints for finding feasible point in
-    # convex polytope
-    G_i = []
-    G_j = []
-    G_v = []
-    G_rows = 0
+    z = cp.Variable(self.n_classes_, name="z")
+
+    # Compute boundaries that defines the convex polytope
     boundaries = np.zeros((self.n_classes_, self.n_classes_))
+    for i in range(self.n_classes_):
+      for j in chain(range(i), range(i + 1, self.n_classes_)):
+        idx = gamma[:, i] > tol  # or ~np.isclose(gamma[:, i], 0)?
+        boundaries[i, j] = np.max(scores[idx, j] - scores[idx, i] + 1,
+                                  initial=0)
+    boundaries -= np.clip(boundaries + boundaries.T - 2, 0,
+                          None) / 2  # in case of numerical issues
+    gaps = np.clip((2 - boundaries.T - boundaries), 1e-2, None)
+
+    # Get cost and build constraints
+    cost = 0
+    constraints = []
 
     for i in range(self.n_classes_):
       for j in chain(range(i), range(i + 1, self.n_classes_)):
-        idx = gamma[:, i] > 0  # or ~np.isclose(gamma[:, i], 0)?
-        boundaries[i, j] = np.max(scores[idx, j] - scores[idx, i] + 1,
-                                  initial=0)
-        G_i.extend([G_rows] * 2)
-        G_j.extend([i, j])
-        G_v.extend([1, -1])
-        G_rows += 1
-    boundaries -= np.clip(
-        boundaries + boundaries.T - 2, 0,
-        None) / 2  # in case of non-optimal LP solution, incl. numerical issues
-    gaps = (2 - boundaries.T -
-            boundaries)[np.where(~np.eye(self.n_classes_, dtype=bool))]
+        # z_j - z_i >= B_ij - 1
+        constraints.append(z[j] - z[i] >= boundaries[i, j] - 1)
+        cost += cp.square(z[j] - z[i] - (boundaries[i, j] - 1)) / gaps[i, j]**2
 
-    G = csc_matrix((G_v, (G_i, G_j)), shape=(G_rows, self.n_classes_))
-    h = 1 - boundaries[np.where(~np.eye(self.n_classes_, dtype=bool))]
+    return cp.Problem(cp.Minimize(cost), constraints)
 
-    # # `scipy.optimize.linprog` interface
-    # sol = linprog(np.zeros(self.n_classes_),
-    #               A_ub=G,
-    #               b_ub=h,
-    #               bounds=None,
-    #               method="highs")
-    # z = res.x
+  def linprog_score_transform_(self, scores, gamma, tol=1e-8):
 
-    # `qpsolvers` interface
-    W = csc_matrix(np.diag(1 / np.clip(gaps, 1e-2, None)**
-                           2))  # ignore small gaps, prevent division by zero
-    z = solve_ls(G, h, G, h, W=W, solver="osqp")
-    assert z is not None, f"No feasible point found; should not happen...\nboundaries =\n{boundaries}"
+    bias = cp.Variable(self.n_classes_, name="bias")
+    constraints = []
 
-    return np.array([0] +
-                    [2 * (z[0] - z[j]) for j in range(1, self.n_classes_)])
+    eq_classes = UndirectedGraph()
+    for s, g in zip(scores, gamma):
+      candidates = np.where(g > tol)[0]  # or use np.isclose?
+      if len(candidates) > 1:
+        # b_i + s_i = b_j + s_j, for i, j in candidates
+        for l, i in enumerate(candidates):
+          for j in candidates[l + 1:]:
+            if not eq_classes.connected(i, j):
+              constraints.append(bias[i] + s[i] == bias[j] + s[j])
+              eq_classes.add_edges([(i, j)])
+              # print(f'b_{i} + {s[i]} = b_{j} + {s[j]}')
+
+    diffs = np.full((self.n_classes_, self.n_classes_), -np.inf)
+    for s, g in zip(scores, gamma):
+      candidates = set(np.where(g > tol)[0])  # or use np.isclose?
+      for i in candidates:
+        for j in range(self.n_classes_):
+          if j not in candidates and not eq_classes.connected(i, j):
+            diffs[i, j] = max(s[j] - s[i], diffs[i, j])
+    for i in range(self.n_classes_):
+      for j in range(self.n_classes_):
+        if i != j and not eq_classes.connected(i, j):
+          constraints.append(bias[i] - bias[j] + tol >= diffs[i, j])
+
+    return cp.Problem(cp.Minimize(0), constraints)
 
   def predict(self, scores, groups):
-    """Output fair class assignments given predictor scores.
+    """Output DP fair class assignments given predictor scores.
 
     Args:
       scores: array-like, shape (n_examples, n_classes)
@@ -297,8 +287,9 @@ class PostProcessorDP(BaseEstimator):
       groups: array-like, shape (n_examples,)
         Group label (zero-indexed) of each example.
     """
-    check_is_fitted(self, "psi_by_group_")
     scores = check_array(scores)
     groups = check_array(groups, ensure_2d=False)
     scores, groups = check_X_y(scores, groups)
-    return np.argmin(2 * (1 - scores) - self.psi_by_group_[groups], axis=1)
+    check_is_fitted(self, "psi_by_group_")
+    # argmin(2 * (1 - s) - \psi) = argmin(-2s - \psi) = argmax(2s + \psi)
+    return np.argmin(-2 * scores - self.psi_by_group_[groups], axis=1)
--- utils.py
+++ utils.py
@@ -1,39 +1,44 @@
+import traceback
+
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
-def error_rate(y, y_preds, groups=None, w=None, n_groups=None):
+def error_rate(y_true, y_preds, groups=None, w=None, n_groups=None):
   """Compute group-weighted error rate."""
   if groups is None or w is None:
-    return np.mean(y != y_preds)
+    return np.mean(y_true != y_preds)
   else:
     if n_groups is None:
       group_names, groups = np.unique(groups, return_inverse=True)
       n_groups = len(group_names)
     return sum([
-        w[a] * np.mean(y[groups == a] != y_preds[groups == a])
+        w[a] * np.mean(y_true[groups == a] != y_preds[groups == a])
         for a in range(n_groups)
     ])
 
 
-def delta_dp(y_preds, groups, n_classes=None, n_groups=None):
-  """Compute DP gap."""
+def delta_dp(y_preds, groups, n_classes=None, n_groups=None, ord=np.inf):
+  """Compute difference in output distributions."""
   if n_classes is None:
     class_names, y_preds = np.unique(y_preds, return_inverse=True)
     n_classes = len(class_names)
   if n_groups is None:
     group_names, groups = np.unique(groups, return_inverse=True)
     n_groups = len(group_names)
-
-  output_dists = np.array([
+  pred_counts = np.array([
       np.bincount(y_preds[groups == a], minlength=n_classes)
       for a in range(n_groups)
   ])
-  output_dists = output_dists / np.sum(output_dists, axis=1, keepdims=True)
-  diffs = np.abs(output_dists[:, None, :] - output_dists[None, :, :])
+  output_dists = pred_counts / np.sum(pred_counts, axis=1, keepdims=True)
+  diffs = np.linalg.norm(output_dists[:, None, :] - output_dists[None, :, :],
+                         ord=ord,
+                         axis=2)
   return np.max(diffs)
 
 
-def perturb(rng, s, bw=1.0, repeat=1, eps=10):
+def perturb(s, repeat, bw=1.0, eps=10, seed=33):
   """Perturb points on the simplex via the dirichlet distribution."""
   s = np.array(s)
   scale = np.min([
@@ -43,4 +48,130 @@ def perturb(rng, s, bw=1.0, repeat=1, eps=10):
                  axis=0)
   scale = (scale * (1 - scale)) / bw
   s = (s + eps * bw) * scale
-  return np.concatenate([rng.dirichlet(r, size=repeat) for r in s], axis=0)
+  return np.concatenate(
+      [np.random.default_rng(seed).dirichlet(r, size=repeat) for r in s],
+      axis=0)
+
+
+# Define some utility functions
+
+
+def postprocess(alpha_seed_and_kwargs,
+                postprocessor_factory,
+                evaluate_fn,
+                probas,
+                labels,
+                groups,
+                n_test,
+                n_post,
+                n_perturbs=0,
+                perturb_fn=None):
+
+  if len(alpha_seed_and_kwargs) == 2:
+    alpha, seed = alpha_seed_and_kwargs
+    kwargs = {}
+  else:
+    alpha, seed, kwargs = alpha_seed_and_kwargs
+
+  # Split the remaining data into post-processing and test data
+  idx_post = np.random.default_rng(seed).choice(np.arange(n_test + n_post),
+                                                size=n_post,
+                                                replace=False)
+  idx_test = np.setdiff1d(np.arange(n_test + n_post), idx_post)
+
+  train_probas_post = probas[idx_post]
+  train_labels_post = labels[idx_post]
+  train_groups_post = groups[idx_post]
+  test_probas = probas[idx_test]
+  test_labels = labels[idx_test]
+  test_groups = groups[idx_test]
+
+  if n_perturbs > 0:
+    train_probas_post = perturb_fn(train_probas_post, n_perturbs)
+    train_labels_post = np.repeat(train_labels_post, n_perturbs)
+    train_groups_post = np.repeat(train_groups_post, n_perturbs)
+    test_probas = perturb_fn(test_probas, 1)
+
+  if alpha == np.inf:
+    # Evaluate the unprocessed model
+    postprocessor = None
+    test_preds = test_probas.argmax(axis=1)
+  else:
+    try:
+      # Post-process the predicted probabilities
+      postprocessor = postprocessor_factory().fit(train_probas_post,
+                                                  train_groups_post,
+                                                  alpha=alpha,
+                                                  **kwargs)
+      # Evaluate the post-processed model
+      test_preds = postprocessor.predict(test_probas, test_groups)
+    except Exception:
+      print(f"Post-processing failed with alpha={alpha} and seed={seed}:")
+      print(traceback.format_exc())
+      return alpha, seed, None, None
+
+  return alpha, seed, evaluate_fn(test_labels, test_preds,
+                                  test_groups), postprocessor
+
+
+def evaluate(test_labels,
+             test_preds,
+             test_groups,
+             n_groups=2,
+             n_classes=2,
+             metrics=None):
+  result = {}
+  result['error_rate'] = error_rate(
+      test_labels,
+      test_preds,
+      test_groups,
+      n_groups=n_groups,
+  )
+  if metrics is not None and 'delta_dp' in metrics:
+    result['delta_dp'] = delta_dp(
+        test_preds,
+        test_groups,
+        n_classes=n_classes,
+        n_groups=n_groups,
+    )
+  if metrics is not None and 'delta_dp_rms' in metrics:
+    result['delta_dp_rms'] = delta_dp(
+        test_preds,
+        test_groups,
+        n_classes=n_classes,
+        n_groups=n_groups,
+        ord=2,
+    ) / np.sqrt(n_classes)
+  return result
+
+
+def plot_results(results, metric):
+  df = pd.DataFrame(results).groupby('alpha').agg(['mean', np.std
+                                                  ]).sort_index(ascending=False)
+  fig, ax = plt.subplots(1, 1, figsize=(3.5, 3))
+  df1 = df[df.index != np.inf]
+  markers, caps, bars = ax.errorbar(
+      df1[metric]['mean'].values,
+      df1['error_rate']['mean'].values,
+      xerr=df1[metric]['std'].values,
+      yerr=df1['error_rate']['std'].values,
+      fmt='o',
+  )
+  [bar.set_alpha(0.4) for bar in bars]
+  if np.inf in df.index:
+    # Plot the unprocessed model
+    df2 = df[df.index == np.inf]
+    markers, caps, bars = ax.errorbar(
+        df2[metric]['mean'].values,
+        df2['error_rate']['mean'].values,
+        xerr=df2[metric]['std'].values,
+        yerr=df2['error_rate']['std'].values,
+        fmt='o',
+        color='tab:blue',
+        markerfacecolor='w',
+    )
+    [bar.set_alpha(0.4) for bar in bars]
+  ax.set_xlabel(metric)
+  ax.set_ylabel("Error rate")
+  ax.grid(True, which="both", zorder=0)
+  return (fig, ax), df
